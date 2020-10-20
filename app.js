d5f5c5f93a588ce1262a8f765ef76993e0813f1e
portfolio <68680118+HaTToek@users.noreply.github.com>
2020-10-20 22:46:37 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use('/', require('./routes/manager'));
 app.use('/posts', require('./routes/posts'));
 
 // Port setting
-const port = 3000;
+var port = server.listen(process.env.PORT || 3000);
 app.listen(port, function(){ 
     console.log('server on! http://localhost:'+port);
 });
